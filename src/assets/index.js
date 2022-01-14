c2aab76cf070c8b44b006c8fe3b5d43f618036f2
Dandi Sabilli Izzulhaq <45232854+dandiabil@users.noreply.github.com>
2022-01-14 23:30:24 +0700

    Update package

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import WelcomeImage from './images/vecteezy_the-family-is-sitting-on-the-sofa-together-and-watching-tv_.jpg';
+import MovieBanner from './images/felix-mooneeram-evlkOfkQ5rE-unsplash.jpg';
+import BoxOfficePoster from './images/MV5BMTc5MDE2ODcwNV5BMl5BanBnXkFtZTgwMzI2NzQ2NzM@._V1_SX300.jpg'
+import AnimationPoster from './images/MV5BZTQyNTU0MDktYTFkYi00ZjNhLWE2ODctMzBkM2U1ZTk3YTMzXkEyXkFqcGdeQXVyNTI4MzE4MDU@._V1_SX300.jpg';
+
+export { WelcomeImage, MovieBanner, BoxOfficePoster, AnimationPoster };
